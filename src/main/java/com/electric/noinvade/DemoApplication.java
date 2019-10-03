3fee5346356cad96510d0121bc881ad6e28a2da4
limingyu <limingyumoon@gmail.com>
2019-10-04 00:32:44 +0800

    second step

--- src/main/java/com/electric/noinvade/DemoApplication.java
+++ src/main/java/com/electric/noinvade/DemoApplication.java
@@ -15,13 +15,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		InfluxDB influxDB = InfluxDBFactory.connect("http://10.9.8.88:8086");
-		String dbName = "nilm";
-		influxDB.setDatabase(dbName);
-		QueryResult queryResult = influxDB.query(new Query("select * from raw_aggr_10s where time >now() -100s"));
-		InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
-		List<Gl> gl =  resultMapper.toPOJO(queryResult, Gl.class);
-		gl.forEach(c -> System.out.println(c));
+//		InfluxDB influxDB = InfluxDBFactory.connect("http://10.9.8.88:8086");
+//		String dbName = "nilm";
+//		influxDB.setDatabase(dbName);
+//		QueryResult queryResult = influxDB.query(new Query("select * from raw_aggr_10s where time >now() -100s"));
+//		InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
+//		List<Gl> gl =  resultMapper.toPOJO(queryResult, Gl.class);
+//		gl.forEach(c -> System.out.println(c));
 
 	}
 	
